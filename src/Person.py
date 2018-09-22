053b4db3a287a98dee6444de92580921a231bd78
avanderg <avanderg@gmail.com>
2018-09-22 16:40:18 -0700

    Added fraction handling in input

--- src/Person.py
+++ src/Person.py
@@ -6,5 +6,16 @@ class Person:
     def createItems(self):
         myItems = input('Input price of ' + self.name + '\'s things: ')
         myItems = myItems.split()
-        myItems = [float(i) for i in myItems]
+        myItems = self.parse(myItems)
         self.owe = sum(myItems)
+
+    def parse(self, myItems):
+        parsedList = []
+        for item in myItems:
+            try:
+                thisItem = float(item)
+                parsedList.append(thisItem)
+            except ValueError:
+                num, denom = item.split('/')
+                parsedList.append(float(num)/float(denom))
+        return parsedList
